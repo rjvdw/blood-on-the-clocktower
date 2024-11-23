dca13d295078974a4a208c72619079f3f4b297cb
Ruud van der Weide <rj@rdcl.dev>
2024-11-23 22:21:33 +0100

    Make source optional

--- src/content/schema/character.ts
+++ src/content/schema/character.ts
@@ -10,7 +10,7 @@ export type NightSchemaType = z.infer<typeof nightSchema>
 export default z.object({
   name: z.string(),
   botcId: z.string().optional(),
-  source: z.string().url(),
+  source: z.string().url().optional(),
   type: z.enum(['Townsfolk', 'Outsider', 'Minion', 'Demon', 'Fabled', 'Traveller']),
   game: z.string().optional(),
   homebrew: z.boolean().default(false),
